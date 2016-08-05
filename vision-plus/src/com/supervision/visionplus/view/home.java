c2ab12d0b35c89c73ba0a7b3e670abda2cea9d09
nidura <niduraprageeth@gmail.com>
2016-08-04 23:37:43 -0700

    sample form

--- vision-plus/src/com/supervision/visionplus/view/home.java
+++ vision-plus/src/com/supervision/visionplus/view/home.java
@@ -35,12 +35,12 @@ public class home extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
 
         displayPanel.setBackground(new java.awt.Color(255, 255, 255));
         displayPanel.setLayout(new java.awt.BorderLayout());
@@ -60,6 +60,9 @@ public class home extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Invoice");
+        jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
 
@@ -140,6 +143,7 @@ public class home extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 
 }
