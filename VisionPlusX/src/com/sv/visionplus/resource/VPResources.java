aedce71b698014ad601f90977859a6b525704e7c
kasun chamara <chamara.kaza@gmail.com>
2016-08-23 12:12:01 -0400

    Home ui done :1234:

--- VisionPlusX/src/com/sv/visionplus/resource/VPResources.java
+++ VisionPlusX/src/com/sv/visionplus/resource/VPResources.java
@@ -17,7 +17,22 @@ public class VPResources {
     public static final String MASTER_SUPPLIER = "icons/master/supplier.png";
     public static final String MASTER_USER = "icons/master/user.png";
     public static final String MASTER_ITEM = "icons/master/item.png";
+    public static final String MASTER_BRANCH = "icons/master/branch.png";
 
+    public static final String TRANSACTION_INVOICE = "icons/transaction/invoice.png";
+    public static final String TRANSACTION_PAYMENT = "icons/transaction/payment.png";
+    public static final String TRANSACTION_GRN = "icons/transaction/grn.png";
+    public static final String TRANSACTION_CHECK = "icons/transaction/check.png";
+    
+    public static final String ACCOUNT_VOUCHER = "icons/account/voucher.png";
+    public static final String ACCOUNT_MONEY = "icons/account/money.png";
+
+    public static final String STOCK_TRANSFER = "icons/stock/transfer.png";
+    public static final String STOCK_ADJUST = "icons/stock/adjust.png";
+    
+    public static final String BACKUP_EXPORT = "icons/backup/export.png";
+    public static final String BACKUP_IMPORT = "icons/backup/import.png";
+    
     public static URL getImageUrl(String path) {
         return VPResources.class.getResource(path);
     }
--- VisionPlusX/src/visionplusx/Home.java
+++ VisionPlusX/src/visionplusx/Home.java
@@ -42,8 +42,41 @@ public class Home extends DefaultMainframe {
         addBand("Item", getImageUrl(MASTER_ITEM));
         addButton("Item", getImageUrl(MASTER_ITEM), DefaultMainframe.ElementPriority.TOP, null);
         
-        addBand("Bank Branch", getImageUrl(MASTER_ITEM));
-        addButton("Bank Branch", getImageUrl(MASTER_ITEM), DefaultMainframe.ElementPriority.TOP, null);
+        addBand("Bank", getImageUrl(MASTER_BRANCH));
+        addButton("Bank Branch", getImageUrl(MASTER_BRANCH), DefaultMainframe.ElementPriority.TOP, null);
+        
+        
+        addTask("Transaction");
+        
+        addBand("Transaction", getImageUrl(TRANSACTION_INVOICE));
+        addButton("Invoice", getImageUrl(TRANSACTION_INVOICE), DefaultMainframe.ElementPriority.TOP, null);
+        addButton("GRN", getImageUrl(TRANSACTION_GRN), DefaultMainframe.ElementPriority.TOP, null);
+        
+        addBand("Payment", getImageUrl(TRANSACTION_PAYMENT));
+        addButton("Customer Payment", getImageUrl(TRANSACTION_PAYMENT), DefaultMainframe.ElementPriority.TOP, null);
+        
+        addBand("Checking", getImageUrl(TRANSACTION_PAYMENT));
+        addButton("Checking", getImageUrl(TRANSACTION_CHECK), DefaultMainframe.ElementPriority.TOP, null);
+       
+        addTask("Account");
+        
+        addBand("Account", getImageUrl(ACCOUNT_MONEY));
+        addButton("Money Collection", getImageUrl(ACCOUNT_MONEY), DefaultMainframe.ElementPriority.TOP, null);
+        addButton("Payment Vouchers", getImageUrl(ACCOUNT_VOUCHER), DefaultMainframe.ElementPriority.TOP, null);
+        
+        addTask("Stock");
+        
+        addBand("Stock", getImageUrl(STOCK_TRANSFER));
+        addButton("stock Adjustmant", getImageUrl(STOCK_ADJUST), DefaultMainframe.ElementPriority.TOP, null);
+        addButton("stock transfer", getImageUrl(STOCK_TRANSFER), DefaultMainframe.ElementPriority.TOP, null);
+        
+        addTask("Backup");
+        
+        addBand("Backup", getImageUrl(BACKUP_EXPORT));
+        addButton("Export", getImageUrl(BACKUP_EXPORT), DefaultMainframe.ElementPriority.TOP, null);
+        addButton("Import", getImageUrl(BACKUP_IMPORT), DefaultMainframe.ElementPriority.TOP, null);
+        
+
     }
 
 }
